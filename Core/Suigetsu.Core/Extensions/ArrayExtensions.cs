da7a5c56534dbc1ead771d3a11f03628f3abba99
Stew <contact@stew.com.br>
2016-02-16 23:12:27 -0300

    Initial commit

---
+++ Core/Suigetsu.Core/Extensions/ArrayExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Suigetsu.Core.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[] Convert<T>(this Array array)
+        {
+            var result = new T[array.Length];
+
+            var fromType = array.GetType().GetElementType();
+            var toType = typeof(T);
+
+            if(toType.IsPrimitive && fromType.IsPrimitive)
+            {
+                var fromSize = Marshal.SizeOf(fromType);
+
+                if(fromSize == Marshal.SizeOf(toType))
+                {
+                    Buffer.BlockCopy(array, 0, result, 0, fromSize * result.Length);
+                }
+                else
+                {
+                    for(var i = 0; i < array.Length; i++)
+                    {
+                        var item = new T[1];
+                        Buffer.BlockCopy(array, i * fromSize, item, 0, 1);
+                        result[i] = item[0];
+                    }
+                }
+            }
+            else
+            {
+                result = (from object v in array
+                          select (T)System.Convert.ChangeType(v, typeof(T))).ToArray();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Core/Tests/Suigetsu.Core.Tests/Extensions/ArrayExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Suigetsu.Core.Extensions;
+
+namespace Suigetsu.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class ArrayExtensionsTests : AssertionHelper
+    {
+        [SetUp]
+        public void Initialize()
+        {
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+        }
+
+        [Test]
+        public void ConvertTest()
+        {
+            var arr = new[] { 10, 20, 30 };
+            var res = arr.Convert<byte>();
+            Expect(res.Convert<int>(), EquivalentTo(arr));
+
+            Expect(new[] { 33, 34 }.Convert<int>(), EquivalentTo(new[] { 33, 34 }));
+
+            Expect(new[] { 'a', 'b' }.Convert<string>(), EquivalentTo(new[] { "a", "b" }));
+
+            Expect(new[] { "a", "b" }.Convert<char>(), EquivalentTo(new[] { 'a', 'b' }));
+        }
+    }
+}
