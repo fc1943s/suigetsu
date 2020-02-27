6d37343fff72958ffaabf07a0cc24b99f821cae2
Filipe <fc1943s@gmail.com>
2020-02-27 00:16:50 -0300

    Fresh start (v2)

--- Core/Suigetsu.Core.Desktop/Net/ServerSocket.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Suigetsu.Core.Desktop.Net
-{
-    public class ServerSocket : BaseSocket
-    {
-        public ServerSocket() : base(SocketType.Server) {}
-
-        public new void Listen(SocketListenerParameters socketListenerParameters) => base.Listen(socketListenerParameters);
-    }
-}
--- Core/Suigetsu.Core.Desktop/Net/SocketWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net.Sockets;
-
-namespace Suigetsu.Core.Desktop.Net
-{
-    public class SocketWrapper : Socket, ISocket
-    {
-        public SocketWrapper() : base(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp) {}
-    }
-}
