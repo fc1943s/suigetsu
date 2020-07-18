711444d77cd0d57452a23bac521844d92d58a4c3
Filipe <fc1943s@gmail.com>
2020-07-18 17:26:48 -0300

    useWindowSize hook

---
+++ src/Suigetsu.UI.Frontend.React/CustomHooks.fs
@@ -0,0 +1,28 @@
+module Suigetsu.UI.Frontend.React
+
+open System
+open Browser
+open Browser.Types
+open Feliz
+
+
+module CustomHooks =
+    let useWindowSize () =
+        let getWindowSize () =
+            {| Width = window.innerWidth
+               Height = window.innerHeight |}
+        let size, setSize = React.useState (getWindowSize ())
+
+        React.useLayoutEffect (fun () ->
+            let updateSize (_event: Event) =
+                setSize (getWindowSize ())
+
+            window.addEventListener ("resize", updateSize)
+
+            { new IDisposable with
+                member _.Dispose () =
+                    window.removeEventListener ("resize", updateSize)
+            }
+        )
+        size
+
