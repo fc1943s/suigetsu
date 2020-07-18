10343ee3ff4fa66ab80123e1feb4503389c33f8f
Filipe <fc1943s@gmail.com>
2020-07-18 16:13:17 -0300

    added useTimeout

---
+++ src/Suigetsu.UI.Frontend.React/Hooks.fs
@@ -0,0 +1,34 @@
+namespace Suigetsu.UI.Frontend.React
+
+open System
+open Fable.Core
+open Fable.React
+
+module Hooks =
+    module Scheduling =
+        type SchedulingType =
+            | Timeout
+            | Interval
+
+        let schedulingFn = function
+            | Timeout -> JS.setTimeout, JS.clearTimeout
+            | Interval -> JS.setInterval, JS.clearInterval
+
+        let useInterval schedulingType fn interval =
+            let savedCallback = Hooks.useRef fn
+
+            Hooks.useEffect (fun () ->
+                savedCallback.current <- fn
+            , [| fn |])
+
+            Hooks.useEffectDisposable (fun () ->
+                let set, clear = schedulingFn schedulingType
+                let onExecute () =
+                    savedCallback.current ()
+
+                let id = set onExecute interval
+
+                { new IDisposable with
+                    member _.Dispose () =
+                        clear id }
+            , [| interval |])
